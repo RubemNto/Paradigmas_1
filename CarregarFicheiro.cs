3672b378593f8aed21f81f3259db819aaa5217b6
Nicholas Zimmermann Sado <76746080+DinoiK@users.noreply.github.com>
2021-01-05 21:53:49 +0000

    Add files via upload
    Classe para carregar os ficheiros dos detalhes do ponto 1.
    
    carregue com 'files.'

---
+++ CarregarFicheiro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ficheiros
+{
+    public class CarregarFicheiro
+
+    {
+        private System.IO.StreamReader _files; 
+
+        public CarregarFicheiro(System.IO.StreamReader files)
+        {
+            _files = files;
+
+        }
+
+        public void Carregar()
+        {
+            int counter = 0;
+            string line1;
+
+            
+            while ((line1 = _files.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line1);
+                counter++;
+            }
+
+
+
+            _files.Close();
+
+            System.Console.WriteLine("There were {0} lines.", counter);
+
+            System.Console.ReadLine();
+        }
+    }
+}
